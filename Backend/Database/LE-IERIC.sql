e80cbc7a6fba310b76d2867b4f34b82631047518
Hernan Ferreira <55634948+HernanFerre@users.noreply.github.com>
2026-08-21 12:48:39 -0300

    Rebuild normalized quiz database schema

--- Backend/Database/LE-IERIC.sql
+++ Backend/Database/LE-IERIC.sql
@@ -1,9 +1,26 @@
-﻿IF OBJECT_ID(N'[__EFMigrationsHistory]') IS NULL
+USE [master];
+GO
+
+IF DB_ID(N'LE-IERIC') IS NULL
 BEGIN
-    CREATE TABLE [__EFMigrationsHistory] (
+    EXEC(N'CREATE DATABASE [LE-IERIC]');
+END;
+GO
+
+USE [LE-IERIC];
+GO
+
+SET NOCOUNT ON;
+SET XACT_ABORT ON;
+GO
+
+IF OBJECT_ID(N'[dbo].[__EFMigrationsHistory]', N'U') IS NULL
+BEGIN
+    CREATE TABLE [dbo].[__EFMigrationsHistory] (
         [MigrationId] nvarchar(150) NOT NULL,
         [ProductVersion] nvarchar(32) NOT NULL,
-        CONSTRAINT [PK___EFMigrationsHistory] PRIMARY KEY ([MigrationId])
+        CONSTRAINT [PK___EFMigrationsHistory]
+            PRIMARY KEY ([MigrationId])
     );
 END;
 GO
@@ -11,7 +28,12 @@ GO
 BEGIN TRANSACTION;
 GO
 
-IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20260728202448_AddQuizPersistence')
+IF NOT EXISTS (
+    SELECT 1
+    FROM [dbo].[__EFMigrationsHistory]
+    WHERE [MigrationId] =
+        N'20260821120000_RebuildQuizPersistence'
+)
 BEGIN
     CREATE TABLE [dbo].[Empresa] (
         [Id] uniqueidentifier NOT NULL,
@@ -25,124 +47,153 @@ BEGIN
         [UsuarioAlta] nvarchar(max) NULL,
         [FechaUpdate] datetime2 NOT NULL,
         [UsuarioUpdate] nvarchar(max) NULL,
-        CONSTRAINT [PK_Empresa] PRIMARY KEY ([Id])
+        CONSTRAINT [PK_Empresa]
+            PRIMARY KEY ([Id])
     );
-END;
-GO
 
-IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20260728202448_AddQuizPersistence')
-BEGIN
-    CREATE TABLE [QuizSesion] (
-        [Id] uniqueidentifier NOT NULL,
-        [CuitEmpresa] char(11) NOT NULL,
-        [Estado] int NOT NULL,
-        [IntentosTotales] int NOT NULL,
-        [IntentosRestantes] int NOT NULL,
-        [FechaCreacionUtc] datetime2 NOT NULL,
-        [FechaExpiracionUtc] datetime2 NOT NULL,
-        [FechaFinalizacionUtc] datetime2 NULL,
-        CONSTRAINT [PK_QuizSesion] PRIMARY KEY ([Id])
+    CREATE TABLE [dbo].[QuizSesion] (
+        [Id] bigint IDENTITY(1,1) NOT NULL,
+        [CuitEmpresa] bigint NOT NULL,
+        [UsuarioId] nvarchar(200) NOT NULL,
+        [Estado] tinyint NOT NULL,
+        [IntentosTotales] tinyint NOT NULL,
+        [FechaCreacion] datetime NOT NULL,
+        [FechaExpiracion] datetime NOT NULL,
+        [FechaFinalizacion] datetime NULL,
+        [BloqueadoHasta] datetime NULL,
+        CONSTRAINT [PK_QuizSesion]
+            PRIMARY KEY ([Id]),
+        CONSTRAINT [CK_QuizSesion_Estado]
+            CHECK ([Estado] BETWEEN 1 AND 4),
+        CONSTRAINT [CK_QuizSesion_IntentosTotales]
+            CHECK ([IntentosTotales] > 0)
     );
-END;
-GO
 
-IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20260728202448_AddQuizPersistence')
-BEGIN
-    CREATE TABLE [QuizCuilVinculado] (
-        [QuizId] uniqueidentifier NOT NULL,
-        [Cuil] char(11) NOT NULL,
-        CONSTRAINT [PK_QuizCuilVinculado] PRIMARY KEY ([QuizId], [Cuil]),
-        CONSTRAINT [FK_QuizCuilVinculado_QuizSesion_QuizId] FOREIGN KEY ([QuizId]) REFERENCES [QuizSesion] ([Id]) ON DELETE CASCADE
+    CREATE TABLE [dbo].[QuizCuilVinculado] (
+        [QuizSesionId] bigint NOT NULL,
+        [Cuil] bigint NOT NULL,
+        CONSTRAINT [PK_QuizCuilVinculado]
+            PRIMARY KEY ([QuizSesionId], [Cuil]),
+        CONSTRAINT [FK_QuizCuilVinculado_QuizSesion_QuizSesionId]
+            FOREIGN KEY ([QuizSesionId])
+            REFERENCES [dbo].[QuizSesion] ([Id])
+            ON DELETE CASCADE
     );
-END;
-GO
 
-IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20260728202448_AddQuizPersistence')
-BEGIN
-    CREATE TABLE [QuizDesafio] (
-        [Id] uniqueidentifier NOT NULL,
-        [QuizId] uniqueidentifier NOT NULL,
-        [Numero] int NOT NULL,
-        [Escenario] int NOT NULL,
+    CREATE TABLE [dbo].[QuizDesafio] (
+        [Id] bigint IDENTITY(1,1) NOT NULL,
+        [QuizSesionId] bigint NOT NULL,
+        [Numero] tinyint NOT NULL,
+        [Escenario] tinyint NOT NULL,
         [EsActual] bit NOT NULL,
-        [FechaCreacionUtc] datetime2 NOT NULL,
-        CONSTRAINT [PK_QuizDesafio] PRIMARY KEY ([Id]),
-        CONSTRAINT [FK_QuizDesafio_QuizSesion_QuizId] FOREIGN KEY ([QuizId]) REFERENCES [QuizSesion] ([Id]) ON DELETE CASCADE
+        [FechaCreacion] datetime NOT NULL,
+        CONSTRAINT [PK_QuizDesafio]
+            PRIMARY KEY ([Id]),
+        CONSTRAINT [FK_QuizDesafio_QuizSesion_QuizSesionId]
+            FOREIGN KEY ([QuizSesionId])
+            REFERENCES [dbo].[QuizSesion] ([Id])
+            ON DELETE CASCADE,
+        CONSTRAINT [CK_QuizDesafio_Numero]
+            CHECK ([Numero] > 0),
+        CONSTRAINT [CK_QuizDesafio_Escenario]
+            CHECK ([Escenario] BETWEEN 1 AND 4)
     );
-END;
-GO
 
-IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20260728202448_AddQuizPersistence')
-BEGIN
-    CREATE TABLE [QuizOpcion] (
-        [QuizDesafioId] uniqueidentifier NOT NULL,
-        [Id] varchar(10) NOT NULL,
-        [Cuil] char(11) NOT NULL,
+    CREATE TABLE [dbo].[QuizOpcion] (
+        [QuizDesafioId] bigint NOT NULL,
+        [CodigoOpcion] tinyint NOT NULL,
+        [Cuil] bigint NULL,
         [EsVinculado] bit NOT NULL,
-        [Orden] int NOT NULL,
-        CONSTRAINT [PK_QuizOpcion] PRIMARY KEY ([QuizDesafioId], [Id]),
-        CONSTRAINT [FK_QuizOpcion_QuizDesafio_QuizDesafioId] FOREIGN KEY ([QuizDesafioId]) REFERENCES [QuizDesafio] ([Id]) ON DELETE CASCADE
+        CONSTRAINT [PK_QuizOpcion]
+            PRIMARY KEY ([QuizDesafioId], [CodigoOpcion]),
+        CONSTRAINT [FK_QuizOpcion_QuizDesafio_QuizDesafioId]
+            FOREIGN KEY ([QuizDesafioId])
+            REFERENCES [dbo].[QuizDesafio] ([Id])
+            ON DELETE CASCADE,
+        CONSTRAINT [CK_QuizOpcion_Codigo]
+            CHECK ([CodigoOpcion] BETWEEN 0 AND 5),
+        CONSTRAINT [CK_QuizOpcion_Cuil]
+            CHECK (
+                (
+                    [CodigoOpcion] BETWEEN 0 AND 3
+                    AND [Cuil] IS NOT NULL
+                )
+                OR
+                (
+                    [CodigoOpcion] BETWEEN 4 AND 5
+                    AND [Cuil] IS NULL
+                )
+            )
     );
-END;
-GO
 
-IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20260728202448_AddQuizPersistence')
-BEGIN
-    CREATE TABLE [QuizRespuesta] (
-        [Id] uniqueidentifier NOT NULL,
-        [QuizDesafioId] uniqueidentifier NOT NULL,
-        [OpcionesSeleccionadas] nvarchar(200) NOT NULL,
+    CREATE TABLE [dbo].[QuizRespuesta] (
+        [Id] bigint IDENTITY(1,1) NOT NULL,
+        [QuizDesafioId] bigint NOT NULL,
         [EsCorrecta] bit NOT NULL,
-        [FechaRespuestaUtc] datetime2 NOT NULL,
-        CONSTRAINT [PK_QuizRespuesta] PRIMARY KEY ([Id]),
-        CONSTRAINT [FK_QuizRespuesta_QuizDesafio_QuizDesafioId] FOREIGN KEY ([QuizDesafioId]) REFERENCES [QuizDesafio] ([Id]) ON DELETE CASCADE
+        [FechaRespuesta] datetime NOT NULL,
+        CONSTRAINT [PK_QuizRespuesta]
+            PRIMARY KEY ([Id]),
+        CONSTRAINT [AK_QuizRespuesta_Id_QuizDesafioId]
+            UNIQUE ([Id], [QuizDesafioId]),
+        CONSTRAINT [FK_QuizRespuesta_QuizDesafio_QuizDesafioId]
+            FOREIGN KEY ([QuizDesafioId])
+            REFERENCES [dbo].[QuizDesafio] ([Id])
+            ON DELETE CASCADE,
+        CONSTRAINT [UX_QuizRespuesta_QuizDesafio]
+            UNIQUE ([QuizDesafioId])
     );
-END;
-GO
 
-IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20260728202448_AddQuizPersistence')
-BEGIN
-    EXEC(N'CREATE UNIQUE INDEX [UX_QuizDesafio_Quiz_Actual] ON [QuizDesafio] ([QuizId]) WHERE [EsActual] = 1');
-END;
-GO
+    CREATE TABLE [dbo].[QuizRespuestaOpcion] (
+        [QuizRespuestaId] bigint NOT NULL,
+        [QuizDesafioId] bigint NOT NULL,
+        [CodigoOpcion] tinyint NOT NULL,
+        CONSTRAINT [PK_QuizRespuestaOpcion]
+            PRIMARY KEY ([QuizRespuestaId], [CodigoOpcion]),
+        CONSTRAINT [FK_QuizRespuestaOpcion_QuizRespuesta]
+            FOREIGN KEY ([QuizRespuestaId], [QuizDesafioId])
+            REFERENCES [dbo].[QuizRespuesta] ([Id], [QuizDesafioId])
+            ON DELETE CASCADE,
+        CONSTRAINT [FK_QuizRespuestaOpcion_QuizOpcion]
+            FOREIGN KEY ([QuizDesafioId], [CodigoOpcion])
+            REFERENCES [dbo].[QuizOpcion] (
+                [QuizDesafioId],
+                [CodigoOpcion]
+            )
+    );
 
-IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20260728202448_AddQuizPersistence')
-BEGIN
-    CREATE UNIQUE INDEX [UX_QuizDesafio_Quiz_Numero] ON [QuizDesafio] ([QuizId], [Numero]);
-END;
-GO
+    CREATE UNIQUE INDEX [UX_QuizDesafio_Sesion_Actual]
+        ON [dbo].[QuizDesafio] ([QuizSesionId])
+        WHERE [EsActual] = 1;
 
-IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20260728202448_AddQuizPersistence')
-BEGIN
-    CREATE UNIQUE INDEX [UX_QuizOpcion_Desafio_Orden] ON [QuizOpcion] ([QuizDesafioId], [Orden]);
-END;
-GO
+    CREATE UNIQUE INDEX [UX_QuizDesafio_Sesion_Numero]
+        ON [dbo].[QuizDesafio] (
+            [QuizSesionId],
+            [Numero]
+        );
 
-IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20260728202448_AddQuizPersistence')
-BEGIN
-    CREATE INDEX [IX_QuizRespuesta_Desafio_Fecha] ON [QuizRespuesta] ([QuizDesafioId], [FechaRespuestaUtc]);
-END;
-GO
+    CREATE INDEX [IX_QuizSesion_Cuit_Bloqueo]
+        ON [dbo].[QuizSesion] (
+            [CuitEmpresa],
+            [Estado],
+            [BloqueadoHasta]
+        );
 
-IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20260728202448_AddQuizPersistence')
-BEGIN
-    CREATE INDEX [IX_QuizSesion_CuitEmpresa] ON [QuizSesion] ([CuitEmpresa]);
-END;
-GO
+    CREATE INDEX [IX_QuizSesion_Usuario_Fecha]
+        ON [dbo].[QuizSesion] (
+            [UsuarioId],
+            [FechaCreacion]
+        );
 
-IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20260728202448_AddQuizPersistence')
-BEGIN
-    CREATE INDEX [IX_QuizSesion_Estado] ON [QuizSesion] ([Estado]);
-END;
-GO
-
-IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20260728202448_AddQuizPersistence')
-BEGIN
-    INSERT INTO [__EFMigrationsHistory] ([MigrationId], [ProductVersion])
-    VALUES (N'20260728202448_AddQuizPersistence', N'7.0.3');
+    INSERT INTO [dbo].[__EFMigrationsHistory] (
+        [MigrationId],
+        [ProductVersion]
+    )
+    VALUES (
+        N'20260821120000_RebuildQuizPersistence',
+        N'7.0.3'
+    );
 END;
 GO
 
 COMMIT;
 GO
-
